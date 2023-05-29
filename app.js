4ea4e626e3b57ad67bb177acb5f96413a9c7cfc6
kedrakk <keaindrak@gmail.com>
2023-05-29 14:19:07 +0630

    remove unused body params

--- app.js
+++ app.js
@@ -24,139 +24,139 @@ app.use(express.static(path.resolve(__dirname, 'front')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(cookieParser());
 
 app.get('/callback', function (req, res) {
-    res.sendFile(__dirname + '/front/index.html');
+  res.sendFile(__dirname + '/front/index.html');
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + 'front/index.html');
+  res.sendFile(__dirname + 'front/index.html');
 });
 
 app.get("/getEnv", function (req, res) {
-    try {
-      if (
-        config.APP_CONFIG.DEMO_APP_CLIENT_ID == undefined ||
-        config.APP_CONFIG.DEMO_APP_CLIENT_ID == null
-      ) {
-        res.status(500).send({
-          error: "Missing Client ID",
-        });
-      } else {
-        res.status(200).send({
-          clientId: config.APP_CONFIG.DEMO_APP_CLIENT_ID,
-          redirectUrl: config.APP_CONFIG.DEMO_APP_CALLBACK_URL,
-          scope: config.APP_CONFIG.DEMO_APP_SCOPES,
-          purpose_id: config.APP_CONFIG.DEMO_APP_PURPOSE_ID,
-          authApiUrl: config.APP_CONFIG.MYINFO_API_AUTHORIZE,
-          subentity: config.APP_CONFIG.DEMO_APP_SUBENTITY_ID,
-        });
-      }
-    } catch (error) {
-      console.log("Error".red, error);
+  try {
+    if (
+      config.APP_CONFIG.DEMO_APP_CLIENT_ID == undefined ||
+      config.APP_CONFIG.DEMO_APP_CLIENT_ID == null
+    ) {
       res.status(500).send({
-        error: error,
+        error: "Missing Client ID",
+      });
+    } else {
+      res.status(200).send({
+        clientId: config.APP_CONFIG.DEMO_APP_CLIENT_ID,
+        redirectUrl: config.APP_CONFIG.DEMO_APP_CALLBACK_URL,
+        scope: config.APP_CONFIG.DEMO_APP_SCOPES,
+        purpose_id: config.APP_CONFIG.DEMO_APP_PURPOSE_ID,
+        authApiUrl: config.APP_CONFIG.MYINFO_API_AUTHORIZE,
+        subentity: config.APP_CONFIG.DEMO_APP_SUBENTITY_ID,
       });
     }
-  });
-
-  app.post("/generateCodeChallenge", async function (req, res, next) {
-    try {
-      // call connector to generate code_challenge and code_verifier
-      let pkceCodePair = connector.generatePKCECodePair();
-      // create a session and store code_challenge and code_verifier pair
-      let sessionId = crypto.randomBytes(16).toString("hex");
-      sessionIdCache[sessionId] = pkceCodePair.codeVerifier;
-  
-      //establish a frontend session with browser to retrieve back code_verifier
-      res.cookie("sid", sessionId);
-      //send code code_challenge to frontend to make /authorize call
-      res.status(200).send(pkceCodePair.codeChallenge);
-    } catch (error) {
-      console.log("Error".red, error);
-      res.status(500).send({
-        error: error,
-      });
-    }
-  });
-
-  app.get("/callback", function (req, res) {
-    res.sendFile(__dirname + `/public/index.html`);
-  });
-
-  function readFiles(dirname, onFileContent, onError) {
-    fs.readdir(dirname, function (err, filenames) {
-      if (err) {
-        onError(err);
-        return;
-      }
-      filenames.forEach(function (filename) {
-        fs.readFile(dirname + filename, "utf8", function (err, content) {
-          if (err) {
-            onError(err);
-            return;
-          }
-          onFileContent(filename, content);
-        });
-      });
+  } catch (error) {
+    console.log("Error".red, error);
+    res.status(500).send({
+      error: error,
     });
   }
+});
 
-  app.post("/getPersonData", async function (req, res, next) {
-    try {
-      // get variables from frontend
-      var authCode = req.body.authCode;
-      //retrieve code verifier from session cache
-      var codeVerifier = sessionIdCache[req.cookies.sid];
-      console.log("Calling MyInfo NodeJs Library...".green);
-  
-      // retrieve private siging key and decode to utf8 from FS
-      let privateSigningKey = fs.readFileSync(
-        config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY,
-        "utf8"
-      );
-  
-      let privateEncryptionKeys = [];
-      // retrieve private encryption keys and decode to utf8 from FS, insert all keys to array
-      readFiles(
-        config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS,
-        (filename, content) => {
-          privateEncryptionKeys.push(content);
-        },
-        (err) => {
-          throw err;
-        }
-      );
-  
-      //call myinfo connector to retrieve data
-      let personData = await connector.getMyInfoPersonData(
-        authCode,
-        codeVerifier,
-        privateSigningKey,
-        privateEncryptionKeys
-      );
-  
-      /* 
-        P/s: Your logic to handle the person data ...
-      */
-      console.log(
-        "--- Sending Person Data From Your-Server (Backend) to Your-Client (Frontend)---:"
-          .green
-      );
-      console.log(JSON.stringify(personData)); // log the data for demonstration purpose only
-      res.status(200).send(personData); //return personData
-    } catch (error) {
-      console.log("---MyInfo NodeJs Library Error---".red);
-      console.log(error);
-      res.status(500).send({
-        error: error,
-      });
+app.post("/generateCodeChallenge", async function (req, res, next) {
+  try {
+    // call connector to generate code_challenge and code_verifier
+    let pkceCodePair = connector.generatePKCECodePair();
+    // create a session and store code_challenge and code_verifier pair
+    let sessionId = crypto.randomBytes(16).toString("hex");
+    sessionIdCache[sessionId] = pkceCodePair.codeVerifier;
+
+    //establish a frontend session with browser to retrieve back code_verifier
+    res.cookie("sid", sessionId);
+    //send code code_challenge to frontend to make /authorize call
+    res.status(200).send(pkceCodePair.codeChallenge);
+  } catch (error) {
+    console.log("Error".red, error);
+    res.status(500).send({
+      error: error,
+    });
+  }
+});
+
+app.get("/callback", function (req, res) {
+  res.sendFile(__dirname + `/public/index.html`);
+});
+
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function (err, filenames) {
+    if (err) {
+      onError(err);
+      return;
     }
+    filenames.forEach(function (filename) {
+      fs.readFile(dirname + filename, "utf8", function (err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
   });
+}
+
+app.post("/getPersonData", async function (req, res, next) {
+  try {
+    // get variables from frontend
+    var authCode = req.body.authCode;
+    //retrieve code verifier from session cache
+    var codeVerifier = sessionIdCache[req.cookies.sid];
+    console.log("Calling MyInfo NodeJs Library...".green);
+
+    // retrieve private siging key and decode to utf8 from FS
+    let privateSigningKey = fs.readFileSync(
+      config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY,
+      "utf8"
+    );
+
+    let privateEncryptionKeys = [];
+    // retrieve private encryption keys and decode to utf8 from FS, insert all keys to array
+    readFiles(
+      config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS,
+      (filename, content) => {
+        privateEncryptionKeys.push(content);
+      },
+      (err) => {
+        throw err;
+      }
+    );
+
+    //call myinfo connector to retrieve data
+    let personData = await connector.getMyInfoPersonData(
+      authCode,
+      codeVerifier,
+      privateSigningKey,
+      privateEncryptionKeys
+    );
+
+    /* 
+      P/s: Your logic to handle the person data ...
+    */
+    console.log(
+      "--- Sending Person Data From Your-Server (Backend) to Your-Client (Frontend)---:"
+        .green
+    );
+    console.log(JSON.stringify(personData)); // log the data for demonstration purpose only
+    res.status(200).send(personData); //return personData
+  } catch (error) {
+    console.log("---MyInfo NodeJs Library Error---".red);
+    console.log(error);
+    res.status(500).send({
+      error: error,
+    });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 })
